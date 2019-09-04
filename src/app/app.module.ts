2960689d8b9dafa9e56eee6ab7cae3c5fde769df
Abdul Gaffar Shaikh <abdul219428@gmail.com>
2019-09-04 14:40:29 +0530

    node_modules folder added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { EmployeeComponent } from "./employee/employee.component";
 
 @NgModule({
   declarations: [AppComponent, EmployeeComponent],
