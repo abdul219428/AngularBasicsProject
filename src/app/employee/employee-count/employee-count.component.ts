f223ad4d86816cabf5759c36b0d68222146cf384
Abdul Gaffar Shaikh <abdul219428@gmail.com>
2019-09-05 21:22:55 +0530

    @Output Basics

--- src/app/employee/employee-count/employee-count.component.ts
+++ src/app/employee/employee-count/employee-count.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit ,Input} from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: "app-employee-count",
-  templateUrl: "./employee-count.component.html",
-  styleUrls: ["./employee-count.component.css"]
+  selector: 'app-employee-count',
+  templateUrl: './employee-count.component.html',
+  styleUrls: ['./employee-count.component.css']
 })
 export class EmployeeCountComponent implements OnInit {
+  selectedRadioButtonValue: string = 'All';
+  @Output()
+  countRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<
+    string
+  >();
   @Input()
   all: number;
   @Input()
   male: number;
-  @Input()  
+  @Input()
   female: number;
+
+  onRadioButtonSelectionChange() {
+    this.countRadioButtonSelectionChanged.emit(this.selectedRadioButtonValue);
+    console.log(this.selectedRadioButtonValue);
+  }
   constructor() {}
 
   ngOnInit() {}
--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,69 +1,73 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-employee-list",
-  templateUrl: "./employee-list.component.html",
-  styleUrls: ["./employee-list.component.css"]
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
   employees: any[];
+  selectedEmployeeCountRadioButton = 'All';
 
   constructor() {
     this.employees = [
       {
-        code: "emp101",
-        name: "Johny Joestar",
-        gender: "Male",
+        code: 'emp101',
+        name: 'Johny Joestar',
+        gender: 'Male',
         annualSalary: 50000,
-        dateOfBirth: "7/21/1894"
+        dateOfBirth: '7/21/1894'
       },
       {
-        code: "emp102",
-        name: "Joseph Joestar",
-        gender: "Male",
+        code: 'emp102',
+        name: 'Joseph Joestar',
+        gender: 'Male',
         annualSalary: 140000,
-        dateOfBirth: "8/21/1954"
+        dateOfBirth: '8/21/1954'
       },
       {
-        code: "emp103",
-        name: "Jotaro Kujo",
-        gender: "Male",
+        code: 'emp103',
+        name: 'Jotaro Kujo',
+        gender: 'Male',
         annualSalary: 250000,
-        dateOfBirth: "8/02/1970"
+        dateOfBirth: '8/02/1970'
       },
       {
-        code: "emp104",
-        name: "Josuke Higashikata",
-        gender: "Male",
+        code: 'emp104',
+        name: 'Josuke Higashikata',
+        gender: 'Male',
         annualSalary: 120000,
-        dateOfBirth: "1/21/1983"
+        dateOfBirth: '1/21/1983'
       },
       {
-        code: "emp105",
-        name: "Giorno Giovanna",
-        gender: "Male",
+        code: 'emp105',
+        name: 'Giorno Giovanna',
+        gender: 'Male',
         annualSalary: 1280000,
-        dateOfBirth: "9/26/1990"
+        dateOfBirth: '9/26/1990'
       },
       {
-        code: "emp106",
-        name: "Jolyne Cujoh",
-        gender: "Female",
+        code: 'emp106',
+        name: 'Jolyne Cujoh',
+        gender: 'Female',
         annualSalary: 150000,
-        dateOfBirth: "1/21/1993"
+        dateOfBirth: '1/21/1993'
       }
     ];
   }
 
-  getTotalEmployeesCount(): number{
+  onEmployeeCountRadioButtonChange(selectedRadioButtonValue: string): void{
+    this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+  }
+  getTotalEmployeesCount(): number {
     return this.employees.length;
-}
-getTotalMaleEmployeesCount(): number{
-  return this.employees.filter(e=>e.gender === "Male").length;
-}
-getTotalFemaleEmployeesCount(): number{
-  return this.employees.filter(e=>e.gender === "Female").length;
-}
+  }
+  getTotalMaleEmployeesCount(): number {
+    return this.employees.filter(e => e.gender === 'Male').length;
+  }
+  getTotalFemaleEmployeesCount(): number {
+    return this.employees.filter(e => e.gender === 'Female').length;
+  }
 
   // getEmployees() {
   //   this.employees = [
