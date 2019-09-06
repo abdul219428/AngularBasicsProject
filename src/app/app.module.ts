6db25bf46372adad72c3beba1e56da41639b6672
Abdul Gaffar Shaikh <abdul219428@gmail.com>
2019-09-06 08:23:02 +0530

    angular routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';  // use npm install @angular/http@latest inorder to import this module
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpModule } from "@angular/http"; // use npm install @angular/http@latest inorder to import this module
 
 // local imports
-import { EmployeeComponent } from './employee/employee.component';
-import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
-import { EmployeeTitlePipe } from './employee/employeeTitle.pipe';
-import { EmployeeCountComponent } from './employee/employee-count/employee-count.component';
-import { SimpleComponent } from './others/simple.component';
-import { EmployeeService } from './employee/employee.service';
+import { EmployeeComponent } from "./employee/employee.component";
+import { EmployeeListComponent } from "./employee/employee-list/employee-list.component";
+import { EmployeeTitlePipe } from "./employee/employeeTitle.pipe";
+import { EmployeeCountComponent } from "./employee/employee-count/employee-count.component";
+import { SimpleComponent } from "./others/simple.component";
+import { EmployeeService } from "./employee/employee.service";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./others/pageNotFound.component";
+import { RouterModule, Routes } from "@angular/router";
 
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "employees", component: EmployeeListComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,9 +28,16 @@ import { EmployeeService } from './employee/employee.service';
     EmployeeListComponent,
     EmployeeTitlePipe,
     EmployeeCountComponent,
-    SimpleComponent
+    SimpleComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
